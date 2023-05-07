efa81627238af7944df00f1d95e30b8375525b3c
Elena <knopka-el@ya.ru>
2023-05-07 21:02:39 +0200

    all the tasks done

---
+++ index.js
@@ -0,0 +1,41 @@
+//задание 1
+let cleanerSalary = 40000;
+let vocalTeacherSalary = 22000;
+console.log(cleanerSalary + vocalTeacherSalary);
+//задание 2
+let a = "Владивосток";
+let b = " ";
+let c = 2000;
+console.log(a + b + c);
+//задание 3
+let messengerTime = 128;
+let fotoLikeTime = 254;
+let vkontakteTime = 137;
+let youTubeTime = 201;
+let procrastinationTime = (messengerTime + fotoLikeTime + vkontakteTime + youTubeTime) / 60;
+console.log('Русалка прокрастинировала ' + procrastinationTime + ' часов.');
+//задание 4
+let pow = 2;
+console.log(pow **= 5);
+//задание 5
+console.log('"Нужно бежать со всех ног, чтобы только оставаться на месте, а чтобы куда-то попасть, надо бежать как минимум вдвое быстрее!"')
+//задание 6
+const greetings = "Привет";
+const name = "Кот";
+alert(greetings + ',' + name);
+//задание 7
+let time;
+time = 34;
+console.log(time);
+console.log('Старт поездки. Осталось минут: ' + time);
+time = time - 15;
+console.log('Немного сториз в соцсетях. Осталось минут:' + time);
+time = time - 10;
+console.log('Немного не новостей, но событий. Осталось минут:' + time);
+time = 0;
+console.log('Вы приехали. Добро пожаловать в Москву');
+
+//задание 8
+let fahrenheit = 451;
+let celsius = (fahrenheit - 32) / 1.8;
+console.log(fahrenheit + 'градуса по Фаренгейту - это ' + celsius + ' градуса по Цельсию.');
